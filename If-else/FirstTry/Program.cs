ebac2a48aca236a760b5af9d70325259102b502a
justkuzan <antonkuzan@gmail.com>
2024-09-10 14:33:30 +0300

    One branch to rule them all

---
+++ If-else/FirstTry/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace firstTry {
+
+    class Program {
+        public static void Main()
+        {
+            System.Console.WriteLine("Enter your age: ");
+            byte user_input = Convert.ToByte(System.Console.ReadLine());
+            /*Тип byte потому что я точно знаю что число, которое нам нужно обработать меньше 255.
+            Получается что нет смысла тратить память на int */
+
+            if (user_input >= 18){
+                System.Console.WriteLine("Great, now keep watching porn you little perv");
+            }
+
+            else {
+                System.Console.WriteLine("Access denied. Calling your family in 5...4...3...2...1...");
+        }
+
+    }
+}
+}
---
+++ If-else/LeapYear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please, enter a year: ");
+            string? input = Console.ReadLine();
+
+             if (int.TryParse(input, out int year))
+             {
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+                    Console.WriteLine("This year is a LEAP year");
+                else
+                    Console.WriteLine("This year is NOT a leap year");
+             }  
+                
+            else
+                Console.WriteLine("Invalid input. Please, enter a valid number.");
+        }
+    }
+}
