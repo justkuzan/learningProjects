e881437ae757569d076a93768e5eac5e4f36d080
justkuzan <antonkuzan@gmail.com>
2024-10-05 18:06:03 +0300

    Fixed GuessTheNumber(new methods structure)

--- .game/GuessTheNumber/Program.cs
+++ .game/GuessTheNumber/Program.cs
@@ -1,67 +1,84 @@
 ﻿using System;
+
 namespace GuessTheNumber
 {
     class Program
     {
+        
+        private static int number;
+        private static int lower;
+        private static int higher;
+        private const int MAX_ATTEMPTS = 5;
+        private static int userInput;
+
         public static void Main()
+        { 
+            PlayGame();
+        }
+
+        public static void PlayGame()
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            Console.InputEncoding = System.Text.Encoding.UTF8;
-
-            while (true)
+            Random rand = new Random();
+            number = rand.Next(0, 101);
+            
+            do
             {
-                int number;
-                int lower, higher;
-                int triesCount = 5;
-                int userInput;
-                Random rand = new Random();
+                lower = Math.Max(0, rand.Next(number - 10, number));
+                higher = Math.Min(100, rand.Next(number + 1, number + 10));
+            } while (higher - lower <= 5);
+            
+            System.Console.WriteLine($"We guessed a number between 0 and 100, it is greater than {lower}, but less than {higher}");
+            System.Console.WriteLine($"What is this number? You have {MAX_ATTEMPTS} attempts to guess");
+            
+            GuessTheNumber(MAX_ATTEMPTS);
+        }
 
-                number = rand.Next(0, 101);
-                
-                do
-                {
-                    lower = Math.Max(0, rand.Next(number - 10, number));
-                    higher = Math.Min(100, rand.Next(number + 1, number + 10));
-                } while (higher - lower <= 5);
+        private static void GuessTheNumber(int triesCount)
+        {
+            System.Console.Write("Your Answer: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
 
-                System.Console.WriteLine($"We guessed a number between 0 and 100, it is greater than {lower}, but less than {higher}");
-                System.Console.WriteLine($"What is this number? You have {triesCount} attempts to guess");
-
-                while (triesCount -- > 0)
-                {
-                    System.Console.Write("Your Answer: ");
-                    userInput = Convert.ToInt32(Console.ReadLine());
-                    if(userInput == number)
-                    {
-                        System.Console.WriteLine($"You are right. It was number {number}");
-                        break;
-                    }
-                    else
-                    {
-                        System.Console.WriteLine("Not correct, try again");
-                        System.Console.WriteLine($"You have {triesCount} attempts left");
-                        System.Console.WriteLine("---------------------------------------");
-                    }
-                }
-
-                if (triesCount < 0)
-                {
-                    System.Console.WriteLine("You lost, alas, but better luck next time. It was a number " + number);
-                }
-                else
-                {
-                    System.Console.WriteLine($"You guessed in {5 - triesCount} attempts");
-                }
-
-                System.Console.WriteLine("Do you want to play again? (y/n)");
-                string? playAgain = Console.ReadLine();
-                if (playAgain?.ToLower() != "y")
-                {
-                    break;
-                }
-                Console.Clear();
+            if (triesCount <= 1)
+            {
+                System.Console.WriteLine("You lost, alas, but better luck next time. It was a number " + number);
+                AskToPlayAgain();
+                return;
+            }
+            
+            if(userInput == number)
+            {
+                System.Console.WriteLine($"You are right. It was number {number}");
+                System.Console.WriteLine($"You guessed in {MAX_ATTEMPTS - triesCount} attempts");
+                AskToPlayAgain();
+                return;
             }
 
+            else
+            {
+                System.Console.WriteLine("Not correct, try again");
+                System.Console.WriteLine($"You have {triesCount - 1} attempts left");
+                System.Console.WriteLine("---------------------------------------");
+                
+                GuessTheNumber(triesCount - 1);
+            }
+        }
+
+        private static void AskToPlayAgain()
+        {
+            System.Console.WriteLine("Do you want to play again? (y/n)");
+            string? playAgain = Console.ReadLine();
+
+            if (playAgain?.ToLower() == "y")
+            {
+                Console.Clear();
+                PlayGame();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Thanks for playing!");
+                Console.ReadKey();
+            }
         }
     }
 }
