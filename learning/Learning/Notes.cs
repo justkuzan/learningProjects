d0fe26acbaa9370682d5e99831f80616d3c9ae2d
HOMEPC\anton <antonkuzan@gmail.com>
2025-03-04 17:25:59 +0200

    Added my first OOP code

--- learning/Learning/Notes.cs
+++ learning/Learning/Notes.cs
@@ -68,4 +68,36 @@ static void Main()
 
 ----------
 
+Первое моё ооп
+
+	enum Color
+	{
+		red,
+		green,
+		yellow,
+		blue
+	}
+
+	class Point
+	{
+		public int x;
+		public int y;
+		public Color color;
+	}
+
+	class Program
+	{
+		static void Main()
+		{
+			Point point = new Point();
+			point.x = 2;
+			point.y = 3;
+			point.color = Color.red;
+
+			Console.WriteLine($"{point.x}, {point.y}, {point.color}");
+		}
+	}
+	
+	----------
+
 */
--- learning/Learning/Program.cs
+++ learning/Learning/Program.cs
@@ -2,28 +2,31 @@
 
 namespace Learning
 {
+	enum Color
+	{
+		red,
+		green,
+		yellow,
+		blue
+	}
+
+	class Point
+	{
+		public int x;
+		public int y;
+		public Color color;
+	}
+
 	class Program
 	{
 		static void Main()
 		{
-			int[,] myArray =
-			{
-				{ 25, 12, 7, 4, 69 },
-				{ 1, 19, 56, 81, 44 },
-				{ 73, 92, 17, 46, 31 }
-			};
+			Point point = new Point();
+			point.x = 2;
+			point.y = 3;
+			point.color = Color.red;
 
-			int height = myArray.GetLength(0); //получаем количество строк массива
-			int width = myArray.GetLength(1); //получаем количество столбцов массива
-
-			for (int y = 0; y < height; y++)
-			{
-				for (int x = 0; x < width; x++)
-				{
-					Console.Write(myArray[y, x] + "\t");
-				}
-				Console.WriteLine();
-			}
+			Console.WriteLine($"{point.x}, {point.y}, {point.color}");
 		}
 	}
 }
